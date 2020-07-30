cb731be7a9536382b5159764e609205bd295d57f
Peman Farajnezhad <peiman.farajnezhad@gmail.com>
2020-07-31 01:39:01 +0430

    get exchange details

--- public/1.js
+++ public/1.js
@@ -9,11 +9,109 @@
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_1__);
+
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
 //
 //
 //
 //
-/* harmony default export */ __webpack_exports__["default"] = ({});
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      loading: false,
+      hasError: false,
+      orderId: "",
+      historyData: {}
+    };
+  },
+  methods: {
+    getHistoryData: function () {
+      var _getHistoryData = _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee() {
+        var result, _e$response$data$mess;
+
+        return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                if (this.orderId) {
+                  _context.next = 3;
+                  break;
+                }
+
+                alert("Please Fill Order ID");
+                return _context.abrupt("return");
+
+              case 3:
+                _context.prev = 3;
+                this.loading = true;
+                this.hasError = false;
+                _context.next = 8;
+                return axios__WEBPACK_IMPORTED_MODULE_1___default.a.get("http://127.0.0.1:8000/api/v1/orders/".concat(this.orderId));
+
+              case 8:
+                result = _context.sent;
+                this.historyData = result.data.data;
+                _context.next = 16;
+                break;
+
+              case 12:
+                _context.prev = 12;
+                _context.t0 = _context["catch"](3);
+                alert((_e$response$data$mess = _context.t0.response.data.message) !== null && _e$response$data$mess !== void 0 ? _e$response$data$mess : _context.t0.message);
+                this.hasError = true;
+
+              case 16:
+                _context.prev = 16;
+                this.loading = false;
+                return _context.finish(16);
+
+              case 19:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee, this, [[3, 12, 16, 19]]);
+      }));
+
+      function getHistoryData() {
+        return _getHistoryData.apply(this, arguments);
+      }
+
+      return getHistoryData;
+    }()
+  }
+});
 
 /***/ }),
 
@@ -32,7 +130,76 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("h1", [_vm._v("History")])
+  return _c("div", [
+    _c("h1", [_vm._v("History")]),
+    _vm._v(" "),
+    _c(
+      "form",
+      {
+        on: {
+          submit: function($event) {
+            $event.preventDefault()
+            return _vm.getHistoryData($event)
+          }
+        }
+      },
+      [
+        _c("input", {
+          directives: [
+            {
+              name: "model",
+              rawName: "v-model",
+              value: _vm.orderId,
+              expression: "orderId"
+            }
+          ],
+          attrs: {
+            type: "number",
+            name: "order",
+            id: "order",
+            placeholder: "Please Enter Order ID"
+          },
+          domProps: { value: _vm.orderId },
+          on: {
+            input: function($event) {
+              if ($event.target.composing) {
+                return
+              }
+              _vm.orderId = $event.target.value
+            }
+          }
+        }),
+        _vm._v(" "),
+        _c("button", { attrs: { type: "submit" } }, [_vm._v("Get Data")])
+      ]
+    ),
+    _vm._v(" "),
+    _vm.historyData && Object.keys(_vm.historyData).length
+      ? _c("div", [
+          _c("h3", [_vm._v("Exchange Detail:")]),
+          _vm._v(" "),
+          _c("div", [
+            _vm._v("\n          from "),
+            _c("b", [_vm._v(_vm._s(_vm.historyData.from_price.currency.abbr))]),
+            _vm._v(" to "),
+            _c("b", [_vm._v(_vm._s(_vm.historyData.to_price.currency.abbr))]),
+            _vm._v(" "),
+            _c("br"),
+            _vm._v(
+              "\n          value: " +
+                _vm._s(_vm.historyData.exchange_total_value) +
+                " "
+            ),
+            _c("br"),
+            _vm._v(
+              "\n          date: " +
+                _vm._s(_vm.historyData.created_at) +
+                "\n      "
+            )
+          ])
+        ])
+      : _vm._e()
+  ])
 }
 var staticRenderFns = []
 render._withStripped = true
--- resources/js/pages/History.vue
+++ resources/js/pages/History.vue
@@ -1,7 +1,61 @@
 <template>
-  <h1>History</h1>
+  <div>
+    <h1>History</h1>
+    <form @submit.prevent="getHistoryData">
+      <input
+        type="number"
+        name="order"
+        id="order"
+        placeholder="Please Enter Order ID"
+        v-model="orderId"
+      />
+      <button type="submit">Get Data</button>
+    </form>
+
+    <div v-if="historyData && Object.keys(historyData).length">
+        <h3>Exchange Detail:</h3>
+
+        <div>
+            from <b>{{historyData.from_price.currency.abbr}}</b> to <b>{{historyData.to_price.currency.abbr}}</b> <br>
+            value: {{historyData.exchange_total_value}} <br>
+            date: {{historyData.created_at}}
+        </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      loading: false,
+      hasError: false,
+      orderId: "",
+      historyData: {},
+    };
+  },
+  methods: {
+    getHistoryData: async function () {
+      if (!this.orderId) {
+        alert("Please Fill Order ID");
+        return;
+      }
+      try {
+        this.loading = true;
+        this.hasError = false;
+        const result = await axios.get(
+          `http://127.0.0.1:8000/api/v1/orders/${this.orderId}`
+        );
+        this.historyData = result.data.data;
+      } catch (e) {
+        alert(e.response.data.message ?? e.message);
+        this.hasError = true;
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+};
 </script>
